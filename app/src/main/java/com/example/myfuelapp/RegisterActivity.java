e325ac4a00e8a92fe9c195d738117abf79ce28f5
TharinduBalasooriya <tharinduudana66@gmail.com>
2022-10-25 01:32:59 +0530

    added UI

--- app/src/main/java/com/example/myfuelapp/RegisterActivity.java
+++ app/src/main/java/com/example/myfuelapp/RegisterActivity.java
@@ -4,14 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.User;
+import remote.APIUtils;
+import remote.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button btnSignUp;
+    UserService userService;
+    List<User> list = new ArrayList<User>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnSignUp = (Button) findViewById(R.id.btnRegisterFuel);
+        userService = APIUtils.getUserService();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUsersList();
+            }
+        });
+    }
+
+    public void getUsersList(){
+        Call<List<User>> call = userService.getUsers();
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                if(response.isSuccessful()){
+                    list = response.body();
+
+                    for(User s : list){
+                        System.out.println(s.getFirstName());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+                Log.e("ERROR: ", t.getMessage());
+            }
+        });
     }
 
     public void onClick(View view) {
