baa2a8d90c5fff44bf57f8e2e12b6fe81e337c99
Isini-Dananjana <isinidan97@gmail.com>
2022-10-26 19:19:07 +0530

    changes added admin

--- app/src/main/java/com/example/myfuelapp/MainActivity.java
+++ app/src/main/java/com/example/myfuelapp/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fuel_station_filter_screen);
+        setContentView(R.layout.activity_add_fuel_loactor);
     }
 
     public void onClick(View view) {
