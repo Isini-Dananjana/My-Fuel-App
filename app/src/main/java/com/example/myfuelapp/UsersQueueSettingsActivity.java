6aaf35c0494fa70487190ccf8fa72a471438f8e0
Maryam-Yumna <maryamyumna@gmail.com>
2022-10-26 19:58:07 +0530

    user-queue

---
+++ app/src/main/java/com/example/myfuelapp/UsersQueueSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfuelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsersQueueSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_queue_settings);
+    }
+}
